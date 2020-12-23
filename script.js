5aa584034ad93b9122c73de3ff31e8f42568f061
Leonardo De Candia <leonardxxxx95@gmail.com>
2020-12-23 22:41:13 +0100

    js

--- script.js
+++ script.js
@@ -1,20 +1,60 @@
 
 // Creo l'oggetto 
 
-let arr = {
-    'user':100,
-    'acquisti': 3000,
-    'contatti': 1000
+let arr = [
+    {
+        'selector' : 'utenti',
+        'value' : 100
+    },
+    {
+        'selector' : 'acquisti',
+        'value' : 3000
+    },
+    {
+        'selector' : 'contatti',
+        'value' : 1000
+    }
+]
+function counterFunction (){
+    let counterWrapper = document.querySelector('#counter-wrapper')
+    if((window.innerHeight + window.pageYOffset) > counterWrapper.offsetTop) {
+        setTimeout(() => {
+            arr.forEach(el => {
+                let selettori = document.querySelector(`[data-counter="${el.selector}"]`)
+                let counter = 0
+                let tempo = 5000/ el.value
+                if(tempo < 50){
+                    counter = el.value/2
+                }
+                let intervallo = setInterval(() => {
+                    selettori.innerHTML = counter
+                    counter++
+                    if(counter > el.value){
+                        clearInterval(intervallo)
+                    }
+                }, tempo)
+                document.removeEventListener('scroll' , counterFunction) 
+            })
+        }, 1500)
+    }
 }
+document.addEventListener('scroll' , counterFunction)
+
+
+
+
+
+
 
 // Seleziono gli elementi
-console.log(arr.user);
-Object.keys(arr).forEach((element , index) => {
-    let contatori = document.querySelector(`#${element}`)
-    console.log(element);
-    console.log(arr.element);
-    contatori.innerHTML = arr.element
-}) ;
+
+// console.log(arr.user);
+// Object.keys(arr).forEach((element , index) => {
+//     let contatori = document.querySelector(`#${element}`)
+//     console.log(element);
+//     console.log(arr.element);
+//     contatori.innerHTML = arr.element
+// }) ;
 
 
 // let acquisti = document.querySelector("#acquisti")
