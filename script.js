30870f87dba642cce594afb48825eb482579d607
Leonardo De Candia <leonardxxxx95@gmail.com>
2020-12-24 13:33:02 +0100

    fine modifiche serch da aggiungere le card

--- script.js
+++ script.js
@@ -42,33 +42,3 @@ document.addEventListener('scroll' , counterFunction)
 
 
 
-
-
-
-
-// Seleziono gli elementi
-
-// console.log(arr.user);
-// Object.keys(arr).forEach((element , index) => {
-//     let contatori = document.querySelector(`#${element}`)
-//     console.log(element);
-//     console.log(arr.element);
-//     contatori.innerHTML = arr.element
-// }) ;
-
-
-// let acquisti = document.querySelector("#acquisti")
-// console.log(acquisti);
-
-// acquisti.innerHTML = 3000;
-// let contatti = document.querySelector("#contatti")
-// contatti.innerHTML = 1000;
-
-// let ninja = document.querySelector("#ninja")
-// console.log(ninja) 
-
-// ninja.addEventListener ("click" , () => { 
-//     let user = document.querySelector("#user")
-//     user.innerHTML = 100;
-
-// })
